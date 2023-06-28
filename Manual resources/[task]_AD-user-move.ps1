3346533350259736b7ab035712f83c917c822a71
Sjoerd van Dijk <s.vandijk@tools4ever.com>
2023-06-28 15:37:59 +0200

    Giuthub Duty

--- Manual resources/[task]_AD-user-move.ps1
+++ Manual resources/[task]_AD-user-move.ps1
@@ -1,17 +1,63 @@
+$ou = $form.ou.Path
+$userPrincipalName = $form.gridUsers.UserPrincipalName
+
 try{
     $adUser = Get-ADuser -Filter { UserPrincipalName -eq $userPrincipalName }
-    HID-Write-Status -Message "Found AD user [$userPrincipalName]" -Event Information
-    HID-Write-Summary -Message "Found AD user [$userPrincipalName]" -Event Information
+    Write-information "Found AD user [$userPrincipalName]" 
+
+     $adUserSID = $([string]$adUser.SID)
+        $Log = @{
+            Action            = "MoveAccount" # optional. ENUM (undefined = default) 
+            System            = "ActiveDirectory" # optional (free format text) 
+            Message           = "Found user with username $userPrincipalName" # required (free format text) 
+            IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+            TargetDisplayName = $userPrincipalName # optional (free format text) 
+            TargetIdentifier  = $adUserSID # optional (free format text) 
+        }
+        #send result back  
+        Write-Information -Tags "Audit" -MessageData $log
 } catch {
-    HID-Write-Status -Message "Could not find AD user [$userPrincipalName]. Error: $($_.Exception.Message)" -Event Error
-    HID-Write-Summary -Message "Failed to find AD user [$userPrincipalName]" -Event Failed
+    Write-error "Could not find AD user [$userPrincipalName]. Error: $($_.Exception.Message)"
+
+       $Log = @{
+            Action            = "MoveAccount" # optional. ENUM (undefined = default) 
+            System            = "ActiveDirectory" # optional (free format text) 
+            Message           = "Could find user with username $userPrincipalName" # required (free format text) 
+            IsError           = $true # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+            TargetDisplayName = $userPrincipalName # optional (free format text) 
+            TargetIdentifier  = "" # optional (free format text) 
+        }
+        #send result back  
+        Write-Information -Tags "Audit" -MessageData $log
 }
 
 try{
     Move-ADObject -Identity $adUser -TargetPath $ou
-    HID-Write-Status -Message "Finished moving AD user [$userPrincipalName] to [$ou]" -Event Success
-    HID-Write-Summary -Message "Successfully moved AD user [$userPrincipalName] to [$ou]" -Event Success
+    Write-information "Finished moving AD user [$userPrincipalName] to [$ou]" 
+
+    Write-information "Finished adding AD user [$userPrincipalName] to AD groups $groupsToAdd"
+    $Log = @{
+            Action            = "MoveAccount" # optional. ENUM (undefined = default) 
+            System            = "ActiveDirectory" # optional (free format text) 
+            Message           = "Moved user $userPrincipalName to OU $ou" # required (free format text) 
+            IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+            TargetDisplayName = $userPrincipalName # optional (free format text) 
+            TargetIdentifier  = $adUserSID # optional (free format text) 
+        }
+    #send result back  
+    Write-Information -Tags "Audit" -MessageData $log
+
 } catch {
-    HID-Write-Status -Message "Could not move AD user [$userPrincipalName] to [$ou]. Error: $($_.Exception.Message)" -Event Error
-    HID-Write-Summary -Message "Failed to move AD user [$userPrincipalName] to [$ou]" -Event Failed
+    Write-error "Could not move AD user [$userPrincipalName] to [$ou]. Error: $($_.Exception.Message)" 
+
+    $Log = @{
+            Action            = "MoveAccount" # optional. ENUM (undefined = default) 
+            System            = "ActiveDirectory" # optional (free format text) 
+            Message           = "Could not move user with username $userPrincipalName" # required (free format text) 
+            IsError           = $true # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+            TargetDisplayName = $userPrincipalName # optional (free format text) 
+            TargetIdentifier  = $adUserSID # optional (free format text) 
+        }
+        #send result back  
+        Write-Information -Tags "Audit" -MessageData $log
 }
